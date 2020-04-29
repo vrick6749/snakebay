ba4da59119d56b63469207dd5b40d4049182e7f4
rick <vrick6749@gmail.com>
2020-04-29 15:35:16 +1000

    snakebay inclass practice

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,43 @@
+class ListingsController < ApplicationController
+
+    before_action :set_listing, only: [:show, :edit, :update, :destroy]
+    def index
+        @listings = Listing.all
+    end
+
+    def show
+      
+    end
+
+    def new
+        @listing = Listing.new
+    end
+
+    def create
+        #finish logic for creating a record
+        Listing.create(listing_params) #in private, to allow files to be accepted as params as relying on rails to automatically match
+    end
+
+    def edit
+
+    end
+
+    def update
+      
+    end
+
+    def destroy
+      
+    end
+
+
+
+    private
+    def set_listing
+        @listing = Listing.find(params[:id])
+    end
+
+    def listing_params
+    params.require(:listing).permit(:title, :description, :breed_id, :sex,:price, :deposit, :date_of_birth, :diet, :picture)
+    end
+end
